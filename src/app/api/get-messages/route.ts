b35183fa93491d1fc0053c4cada4c339eca22d1a
Devansh Rai <devanshrai2385@gmail.com>
2026-08-02 22:52:26 +0530

    add api route to fetch messages

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,63 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/user.model";
+import { User } from "next-auth";
+import { RequestStore } from "next/dist/server/app-render/work-unit-async-storage.external";
+import mongoose from "mongoose";
+import { Types } from "mongoose";
+
+export async function GET(request : Request){
+    await dbConnect()
+
+    const session = await getServerSession(authOptions);
+    const user : User = session?.user;
+
+    if(!session || !user){
+        return Response.json(
+            {
+                success : false,
+                message : "Not Authenticated"
+            },
+            {status : 401}
+        )
+    }
+
+    const userId = new mongoose.Types.ObjectId(user._id);
+    try{
+        const user = await UserModel.aggregate([
+            { $match : { id : userId} },
+            { $unwind : "$messages" },
+            { $sort : { "messages.createdAt" : -1 } },
+            { $group : { _id : "$_id", messages : {$push : "$messages"}} }
+        ])
+        
+        if(!user || user.length === 0){
+            return Response.json(
+                {
+                    success : false,
+                    message : "User not found"
+                },
+                {status : 401}
+            )
+        }
+
+        return Response.json(
+            {
+                success : true,
+                message : "Messages fetched successfully",
+                data : user[0].messages
+            },
+            {status : 200}
+        )
+    }
+    catch(error){
+        return Response.json(
+            {
+                success : false,
+                message : "Error fetching messages",
+            },
+            {status : 500}
+        )
+    }
+}
