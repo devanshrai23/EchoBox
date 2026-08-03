a801e8f0c20b379f5b494a762b4cc0b9ad84480d
Devansh Rai <devanshrai2385@gmail.com>
2026-08-04 00:36:14 +0530

    feat: add zod schemas for message validation

--- src/schemas/messageSchema.ts
+++ src/schemas/messageSchema.ts
@@ -1,8 +1,8 @@
-import {z} from "zod";
+import { z } from 'zod';
 
 export const messageSchema = z.object({
-    content : z
+  content: z
     .string()
-    .min(10, {message : "Message must be at least 10 characters long"})
-    .max(300, {message : "Message must be at most 300 characters long"})
-})
+    .min(10, { message: 'Content must be at least 10 characters.' })
+    .max(300, { message: 'Content must be no longer than 300 characters.' }),
+});
