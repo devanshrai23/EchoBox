1f425e7d11f048b0f9ba21c03850c008047b88d7
Devansh Rai <devanshrai2385@gmail.com>
2026-08-07 00:38:25 +0530

    Correct API status code logic to return HTTP 200 instead of 400 for valid unique usernames

--- src/app/api/check-unique-username/route.ts
+++ src/app/api/check-unique-username/route.ts
@@ -52,10 +52,10 @@ export async function GET(request: Request) {
 		}
 		return Response.json(
 			{
-				success: false,
+				success: true,
 				message: 'username is unique',
 			},
-			{ status: 400 },
+			{ status: 200 },
 		);
 	} catch (error) {
 		console.error('Error checking username', error);
